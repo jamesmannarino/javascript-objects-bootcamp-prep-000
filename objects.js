0aaa599858330cccbbe6659a13f1b0143c5efd88
James Mannarino <jamesmannarino@gmail.com>
2020-05-08 18:36:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
-var playlist = {artistName: "songTitle"}
+var playlist = new Object ({artistName: 'song'})
 
-function updatePlaylist(playlist, artistName, songTitle) {
- playlist[artistName] = songTitle
- return playlist
+function updatePlaylist (playList, artistName, songTitle) {
+  playList[artistName] = songTitle
+  return playList
 }
 
-function removeFromPlaylist(playlist, artistName) {
+function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName]
   return playlist
 }
